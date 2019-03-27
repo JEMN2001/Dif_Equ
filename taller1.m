a6dbd2be247cd3110adabf458b795a43a7a688b0
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-03-27 11:04:03 -0500

    a

--- taller1.m
+++ taller1.m
@@ -1,11 +1,11 @@
 clear all
 t0 = 0;
 y0 = 1;
-h = 0.01;
+h = 0.05;
 n = (2/h)+2;
 f = @(t,y) 1-t+4*y;
 ana = @(t) (1/16)*(4*t-3+19*exp(4*t));
-[tf,yf] = Euler_m(f,t0,y0,n,h);
+[tf,yf] = Runge_Kutta(f,t0,y0,n,h);
 rap = yf;
 rap(1)
 rap((1/h)*0.1+1)
